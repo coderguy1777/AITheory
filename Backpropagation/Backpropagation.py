133f9440efd1d4faad5c6ebac11fe84513ad7a4c
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2018-10-31 08:26:12 -0400

    Add files via upload

--- Backpropagation/Backpropagation.py
+++ Backpropagation/Backpropagation.py
@@ -1,9 +1,11 @@
 import numpy as np
+import math
+import random
 
 neuraltraining_data = [
-    [0,1,1],
-    [1,0,1],
+    [0,1,0],
+    [1,1,0],
     [0,0,1],
+    [0,1,1]
 ]
 
-print(neuraltraining_data)
