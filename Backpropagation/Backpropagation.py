db91496a83354e50574f15bd69c0a4ad1745b67d
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2018-10-31 08:37:17 -0400

    Add files via upload

--- Backpropagation/Backpropagation.py
+++ Backpropagation/Backpropagation.py
@@ -3,9 +3,13 @@ import math
 import random
 
 neuraltraining_data = [
-    [0,1,0],
-    [1,1,0],
-    [0,0,1],
-    [0,1,1]
+    [0, 1, 0],
+    [1, 1, 0],
+    [0, 0, 1],
+    [0, 1, 1]
 ]
+
+weight = np.arange(1, 40000)
+
+np.random.seed(200)
 
