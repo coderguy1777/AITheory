8f8dfa86fa5e26c9a2a3d542cf6cd982e2e8dbde
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2018-12-06 05:56:12 -0800

    Add files via upload

--- Backpropagation/BackProp.py
+++ Backpropagation/BackProp.py
@@ -1,36 +1,97 @@
 import numpy as np
-x = np.array(([1, 0], [0, 1], [0, 0], [1, 1]), dtype=float)
 
-ys = x/np.amax(x, axis=0)
-class Neuron:
+x = [
+      [1, 0],
+      [0, 1],
+      [1, 0],
+      [0, 0],
+ ]
+
+idealout = [1, 0, 1, 0]
+
+l = []
+
+for iy in x:
+    for iz in iy:
+        l.append(iz)
+
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+def sigderivative(x):
+    return sigmoid(x)/(1.0 + sigmoid(x))
+
+
+# Defines a Class for the Hidden Values to be stored in, along with inputs, outputs, etc.
+# Using this class, these values are mainly stored as tuples in this case.
+class Inputvals:
+    def __init__(self, values, name):
+        self.name = name
+        self.values = values
+
+    def valreturn(self):
+        return self.values
+
+    def nameval(self):
+        return self.name
+
+
+class Neuron(object):
     def __init__(self):
-        self.hiddensize = 10
-        self.inputsize = -10
-        self.outsize = 1
-        self.w1 = np.random.uniform(self.hiddensize, self.inputsize)
-        self.w2 = np.random.uniform(self.hiddensize, self.outsize)
-        self.b = 1
-        self.target = 1
+        self.learningrate = 0.7
+        self.learningmomentum = 0.3
+        self.weights = np.random.uniform(size=len(x) * 2) * 20 - 10
+        self.bias = 1
+        self.predic = 1
+        self.weight2 = np.random.uniform(size=len(x) * 2) * 20 - 10
+        self.fowardprop = {}
+        self.h1arr = []
+        self.outvals = []
+        self.errors = []
+        self.value = "Hidden Values"
+
+    def calc11(self, x):
+        z1 = np.dot(x, self.weights) + self.bias
+        z2 = np.dot(x, self.weight2) + self.bias
+        h1val = z1 + z2
+        self.h1arr.append(self.sighvals(h1val))
+        return self.sighvals(h1val)
+
+    def sighvals(self, x1):
+        return sigmoid(x1)
+
+    def sumout(self, x2):
+        for i in self.h1arr:
+            x1 = x2 * self.weight2 - i
+            return sum(x1)
+
+    def sigmoidofact(self, x3):
+        self.outvals.append(sigmoid(x3))
+        return sigmoid(x3)
+
+    def fowardpropvals(self):
+        for i in self.h1arr:
+            self.fowardprop[self.value] = i
+        return self.fowardprop
+
+    def backprop(self):
+        for i in self.outvals:
+           self.error = idealout - i
+           self.errors.append(self.error)
+        return self.errors
+
+    def sigderive(self, x4):
+        return sigderivative(x4)
 
 
-    def sigmoid(self, s):
-        return 1 / (1 + np.exp(-s))
+hava = []
+o = Neuron()
+u = 0
+for i in l:
+    u = o.calc11(l[i])
 
-    def h1values(self, inptut):
-        z = inptut * self.w1 + inptut * self.w2 + self.b
-        ss = self.sigmoid(z)
-        us = np.dot(ss, self.w2)
-        return self.sigmoid(us)
-
-    def error(self, prediction):
-        return (prediction - self.target) ** 2
-
-    def test2(self, ideal):
-        s = np.mean(np.square(ideal))
-        return s
-
-
-test = Neuron()
-test2 = test.h1values(ys)
-
-print(test2)
+print(u)
+xx = o.sumout(u)
+xy = o.sigmoidofact(xx)
+print(xx)
+print(xy)
